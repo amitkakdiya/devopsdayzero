22cefdba0f0985036b9083f2811a5ce74efecd93
amitkakdiya <amit.kakdiya@gmail.com>
2026-04-23 08:55:03 +0530

    Update output messages in helloword.java

--- helloword.java
+++ helloword.java
@@ -1,5 +1,6 @@
 public class helloword {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Batch 42 & 43 Student");
+        System.out.println("Hello, Amit 43 Batch Mohit Amit Yogesh");
     }
 }
